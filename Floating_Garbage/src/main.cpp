1b310f5344317c66d500fd1005eecfb65634a2ec
Greatreyhan <greatreyhan@students.undip.ac.id>
2023-08-22 20:26:45 +0700

    2 temp sensor with lcd

--- Floating_Garbage/src/main.cpp
+++ Floating_Garbage/src/main.cpp
@@ -1,26 +1,35 @@
 #include "max6675.h"
+#include <LiquidCrystal_I2C.h>
 
-int thermoDO = 4;
-int thermoCS = 5;
-int thermoCLK = 6;
+int thermoDOA = 4;
+int thermoCSA = 5;
+int thermoCLKA = 6;
+int thermoDOB = 7;
+int thermoCSB = 8;
+int thermoCLKB = 9;
 
-MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
+LiquidCrystal_I2C lcd(0x27,20,4);
+
+MAX6675 thermocoupleA(thermoCLKA, thermoCSA, thermoDOA);
+MAX6675 thermocoupleB(thermoCLKB, thermoCSB, thermoDOB);
 
 void setup() {
   Serial.begin(9600);
-
-  Serial.println("MAX6675 test");
-  // wait for MAX chip to stabilize
-  delay(500);
+  lcd.init();                   
+  lcd.backlight();
 }
 
 void loop() {
-  // basic readout test, just print the current temp
-  
-  Serial.print("C = "); 
-  Serial.println(thermocouple.readCelsius());
-  Serial.print("F = ");
-  Serial.println(thermocouple.readFahrenheit());
+  lcd.clear();
+
+  lcd.setCursor(0,0);
+  lcd.print("C = ");
+  lcd.setCursor(4,0);
+  lcd.print(thermocoupleA.readCelsius());
+  lcd.setCursor(0,1);
+  lcd.print("C = ");
+  lcd.setCursor(4,1);
+  lcd.print(thermocoupleB.readCelsius());
  
   // For the MAX6675 to update, you must delay AT LEAST 250ms between reads!
   delay(1000);
